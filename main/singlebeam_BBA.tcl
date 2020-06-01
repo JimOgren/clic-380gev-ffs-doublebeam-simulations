64d8ba3e73a24a3a68552d362eeb195b0e339110
jogren <jim.ogren@cern.ch>
2020-06-01 14:04:38 +0200

    Updated scripts for running BBA scripts.

--- main/singlebeam_BBA.tcl
+++ main/singlebeam_BBA.tcl
@@ -12,10 +12,10 @@ global guinea_exec
 set run_on_lxplus 0
 
 if { $run_on_lxplus } {
-   set script_dir /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/Clean_scripts/clic-380gev-ffs-singlebeam-simulations
+   set script_dir /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/Clean_scripts/clic-380gev-ffs-doublebeam-simulations
    set guinea_exec /afs/cern.ch/eng/sl/clic-code/lx64slc5/guinea-pig/bin/guinea-old
 } else {
-   set script_dir /home/jim/GIT/clic-380gev-ffs-singlebeam-simulations
+   set script_dir /home/jim/GIT/clic-380gev-ffs-doublebeam-simulations
    set guinea_exec /home/jim/bin/guinea
 }
 
@@ -63,7 +63,7 @@ array set args {
 	deltae 0.001
 	machine 1
 	loadmachine 0
-	measure_response 0
+	measure_response 1
 	wdisp 0.71
 	iterdts 30
 	gaindts 0.5
