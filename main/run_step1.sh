402b9317df896c3f83f52a1889bfd8ae69c6b3f2
jogren <jim.ogren@cern.ch>
2020-06-08 13:10:18 +0200

    Copy parallel execution files

--- main/run_step1.sh
+++ main/run_step1.sh
@@ -5,4 +5,6 @@ source /cvmfs/clicbp.cern.ch/x86_64-slc6-gcc62-opt/setup.sh
 mkdir -p temp_run
 cd temp_run
 cp ../doublebeam_tuning_step1.tcl .
+cp ../run_guinea_parallel* .
+
 placet doublebeam_tuning_step1.tcl machine $1
