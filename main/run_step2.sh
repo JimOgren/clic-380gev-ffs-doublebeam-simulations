4ea56a5007a474a7c1f0d255a221ad26c682acfc
jogren <jim.ogren@cern.ch>
2020-06-08 13:45:32 +0200

    Scripts for running step #

---
+++ main/run_step2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# If running on lxplus or HTCondor, uncomment to setup placet etc.:
+#source /cvmfs/clicbp.cern.ch/x86_64-slc6-gcc62-opt/setup.sh
+
+mkdir -p temp_run
+cd temp_run
+cp ../doublebeam_tuning_step2.tcl .
+cp ../run_guinea_parallel* .
+
+placet doublebeam_tuning_step2.tcl machine $1
