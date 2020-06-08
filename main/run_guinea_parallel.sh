9b657c09c9484ba483d2fb607c32cee09b356c22
jogren <jim.ogren@cern.ch>
2020-06-08 13:44:59 +0200

    Fixed temp folder bug

--- main/run_guinea_parallel.sh
+++ main/run_guinea_parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mkdir -p temp_$1
-cd temp_$1
+mkdir -p temp_$3
+cd temp_$3
 cp ../run_guinea_parallel.tcl .
 placet run_guinea_parallel.tcl thick_beam $1 pairs $2 beam_case $3
 cd ../
-rm -rf temp_$1
+rm -rf temp_$3
